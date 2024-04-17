ea523b6a9cb50479e1da461281d180c75165bbc3
joaovitor <joaovitordlg23101@gmail.com>
2024-04-17 14:00:12 -0300

    feat: Integrando o front com o back

--- FRONT-END/Html/Cadastro/script.js
+++ FRONT-END/Html/Cadastro/script.js
@@ -1,16 +1,78 @@
+const cpf = document.querySelector("#cpf");
 function formatarCEP(){
-    var input = document.querySelector("#cep");
-    var cep = input.value.replace(/\D/g, '');
+  const cep =  document.querySelector('#cep');
 
-    if (cep.length > 5) {
-      cep = cep.substring(0, 5) + '-' + cep.substring(5);
-    }
+  let inputLength = cep.value.length
 
-    input.value = cep;
+  if(inputLength === 5 ){
+    cep.value+= "-"
+  }
 }
-const buscarCEP = () =>{
-    var cep = document.querySelector("#cep").value
-    var url = `viacep.com.br/ws/${cep}/json/`
-    console.log(cep);
-    console.log("teste")
+function formatarCPF() {
+  const cpf = document.querySelector("#cpf")
+  let inputLength = cpf.value.length;
+  if(inputLength === 3 || inputLength === 7){
+    cpf.value +=".";
+  }
+  else if(inputLength === 11) {
+    cpf.value +="-";
+  }
+}
+function buscarCEP(){
+  const cep =  document.querySelector('#cep');
+  var url = `https://viacep.com.br/ws/${cep.value}/json/`
+  fetch(url)
+    .then((res) => res.json())
+    .then(({bairro, logradouro, uf}) =>{
+        if(bairro === undefined){
+          alert("CEP invalido")
+          document.querySelector("#cep").value = "";
+        }
+        else{
+          document.querySelector("#bairro").value = bairro
+          document.querySelector("#rua").value = logradouro
+          document.querySelector("#uf").value = uf
+        }
+        
+        })
+      .catch((error) =>{
+            alert("Ocorreu um erro ao buscar o cep")
+            console.log(error)
+  })
+}
+const cadastrarUsuario = () =>{
+  const url = "http://localhost:8080/user"
+
+  const nome = document.getElementById("nome");
+  const cpf = document.getElementById("cpf");
+  const email = document.getElementById("email");
+  const senha = document.getElementById("senha");
+  const genero = document.getElementById("genero")
+  const bairro = document.getElementById("bairro");
+  const numero = document.getElementById("numero");
+  const rua = document.getElementById("rua");
+  const uf = document.getElementById("uf");
+  
+  const json = JSON.stringify({
+    name: nome.value,
+    cpf: cpf.value,
+    email: email.value,
+    password: senha.value,
+    gender: genero.value,
+    id_address: {
+      neighborhood: bairro.value,
+      street: rua.value,
+      uf: uf.value
+    }
+  })
+  fetch(url,{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: json
+    })
+    .then((response)=> response.json())
+    .then((res)=>(console.log(res)))
+    .catch((error)=>console.log(error))
 }
