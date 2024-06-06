8156dc958f0d7305d64cbb622d6c865988400b01
João Vitor De Lima <88281625+joaolima16@users.noreply.github.com>
2024-06-06 16:47:59 -0300

    Delete backend directory

--- backend/pi/src/main/java/com/example/projeto_pi/pi/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.projeto_pi.pi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.projeto_pi.pi.entities.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long>{
-    
-}
